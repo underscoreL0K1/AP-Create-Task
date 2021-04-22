ce01cf1d2fb76e2129b2a56010ab865530c2dfe8
Loki <71656278+underscoreL0K1@users.noreply.github.com>
2021-04-22 11:21:21 -0400

    Update EventListener.java

--- src/l0k1/Ap/EventListener.java
+++ src/l0k1/Ap/EventListener.java
@@ -9,18 +9,17 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class EventListener extends ListenerAdapter {
 	
 	
-	public static ArrayList<String> ExcemptUsers = new ArrayList<String>();	
+	//public static ArrayList<String> ExcemptUsers = new ArrayList<String>();	
 	public static ArrayList<String> FilteredPhrases = new ArrayList<String>();	
 	
 	
 	public static void sendMsg(MessageReceivedEvent event, String reason) {
 		if (reason == "commanded") {
-			event.getChannel().sendMessage("command excectued").queue();
+			event.getChannel().sendMessage("Command Registered").queue();
 		} else if (reason == "filtered") {
 			event.getChannel().sendMessage("Sorry, that phrase is blocked by the chat filter").queue();
-		}
+		} 
 	}
-	
 	
 	
 	public static void addToList(String cmd) {
@@ -31,49 +30,10 @@ public class EventListener extends ListenerAdapter {
 			System.out.print(FilteredPhrases);
 		} 
 	}
-		
-	
-	
-	public static void messageDelete(MessageReceivedEvent event) {
-		if (event.getAuthor().isBot()) {
-			return;
-		}
-		
-		event.getMessage().delete().queue();
-		
-		
-		
-	}
-	
-	
-	
-		
-		
-	
-		
 
-	
-	
-	
 	
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
-		//event.getMessage().delete().queue();
-		//System.out.print("worked");
-		/*
-		if (event.getAuthor().isBot()) {
-			return;
-		}
-		
-		String msg = event.getMessage().getContentRaw();
-		
-		//TextChannel channel = event.getTextChannel()
-		
-		event.getChannel().sendMessage(msg).queue();
-		System.out.print(msg);
-		
-		messageDelete(event);
-		*/
 		
 		if (event.getAuthor().isBot()) {
 			return;
@@ -81,22 +41,18 @@ public class EventListener extends ListenerAdapter {
 		
 		String msg = event.getMessage().getContentRaw();
 		
-		
-		if (FilteredPhrases.contains(msg)) {
-			sendMsg(event, "filtered");
-			messageDelete(event);
+		for (String i : FilteredPhrases) {
+			if (msg.contains(i)) {
+				sendMsg(event, "filtered");
+				event.getMessage().delete().queue();
 
+			}
 		}
-		
 		
 		if (msg.contains("%")) {
 		addToList(msg);
 		sendMsg(event, "commanded");
 		}
 		
-		
-		
-		
-		
-		 	}
+	}
 }
